8f2b3a5ecbc03bb37951bb131c5f47a5de8351d0
Andrey Yaroslavtsev <messyar@gmail.com>
2025-05-23 09:45:02 +0500

    Add read_root view with docs url

--- movie-catalog/main.py
+++ movie-catalog/main.py
@@ -1,5 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI(
     title="Movie Catalog",
 )
+
+
+@app.get("/")
+def read_root(
+    request: Request,
+    name: str = "Anonym",
+):
+    docs_url = request.url.replace(
+        path="/docs",
+        query="",
+    )
+    return {
+        "message": f"{name}, welcome to Movie Catalog app!",
+        "docs": str(docs_url),
+    }
