4e5a9092f1c3fca855d672f169c0c7c30a33c40a
Andrey Yaroslavtsev <messyar@gmail.com>
2025-05-26 09:56:30 +0500

    Add view to create movie

--- movie-catalog/api/api_v1/movies/views.py
+++ movie-catalog/api/api_v1/movies/views.py
@@ -1,6 +1,9 @@
+import datetime
+import random
+from annotated_types import Len, Interval
 from typing import Annotated
 
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, status, Form
 
 from api.api_v1.movies.crud import MOVIE_LIST
 from api.api_v1.movies.dependencies import get_movie_by_id
@@ -18,6 +21,32 @@ router = APIRouter(
 )
 def get_movies():
     return MOVIE_LIST
+
+
+def generate_movie_id():
+    return random.randint(5, 100)
+
+
+@router.post(
+    "/",
+    response_model=Movie,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_movie(
+    name: Annotated[str, Len(5, 150), Form()],
+    description: Annotated[str, Len(10, 250), Form()],
+    year: Annotated[int, Interval(ge=1895), Form()],
+    rating: Annotated[int, Interval(ge=1, le=10), Form()],
+    age_limit: Annotated[int, Interval(ge=0, le=18), Form()] = 18,
+):
+    return Movie(
+        id=generate_movie_id(),
+        name=name,
+        description=description,
+        year=year,
+        rating=rating,
+        age_limit=age_limit,
+    )
 
 
 @router.get("/{movie_id}", response_model=Movie)
