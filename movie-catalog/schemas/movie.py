294bd583acaa349fd8b8c284952f139bf9544bb1
Andrey Yaroslavtsev <messyar@gmail.com>
2025-05-26 10:11:26 +0500

    Change movie base scheme, add scheme to create movie

--- movie-catalog/schemas/movie.py
+++ movie-catalog/schemas/movie.py
@@ -1,13 +1,39 @@
+from typing import Annotated
+
+from annotated_types import Len, Interval
 from pydantic import BaseModel
 
 
 class MovieBase(BaseModel):
-    id: int
     name: str
     description: str
 
 
+class MovieCreate(MovieBase):
+    name: Annotated[
+        str,
+        Len(5, 150),
+    ]
+    description: Annotated[
+        str,
+        Len(10, 250),
+    ]
+    year: Annotated[
+        int,
+        Interval(ge=1895),  # 1895 is the year of the first movie
+    ]
+    rating: Annotated[
+        int,
+        Interval(ge=1, le=10),
+    ]
+    age_limit: Annotated[
+        int,
+        Interval(ge=0, le=18),
+    ]
+
+
 class Movie(MovieBase):
+    id: int
     year: int
     rating: float
     age_limit: int
